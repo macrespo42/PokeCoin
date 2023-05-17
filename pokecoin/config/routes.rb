f964d3146cf6135cffbd7ec55e56d7b17a2e9045
macrespo42 <macrespo@student.42.fr>
2023-05-17 18:00:03 +0200

    feat: end of TT, basic homepage

--- pokecoin/config/routes.rb
+++ pokecoin/config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
+  # get '/pokemon', to: 'pokemons#index'
+  # get '/pokemon:id', to: 'pokemons#show'
+  # get '/pokemon:id/checkout', to: 'pokemons#show'
+  # put '/pokemon:id/buy', to: 'pokemons#edit'
+  # put '/pokemon:id/sell', to: 'pokemons#edit'
+  # get '/me', to: 'users#index'
+  # get '/me/add', to: 'users#edit'
+  # put '/me/', to: 'users#update'
+  # get '/me/transaction', to: 'transaction#index'
 end
