54da94208856862d7ae9fc0e2414fd29f4db978d
macrespo42 <macrespo@student.42.fr>
2023-05-17 16:50:21 +0200

    feat: add seed - install gem faker - generate users and pokemons

--- pokecoin/db/seeds.rb
+++ pokecoin/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+
+10.times do
+  User.create(email: Faker::Internet.email,
+              password: '1234',
+              balance: Faker::Number.between(from: 0.0, to: 1_000_000.0).round(2))
+end
+
+20.times do
+  Pokemon.create(name: Faker::Games::Pokemon.name,
+                 image_path: '../assets/images/pika.jpg',
+                 weigth: rand(1..100),
+                 height: rand(1..30),
+                 price: Faker::Number.between(from: 0.0, to: 1_000_000.0).round(2),
+                 user: User.order('RANDOM()').first)
+end
